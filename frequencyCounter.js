ed17bc4b7ee86bc8483f657a736c05d817078f2a
Brian Ruff <thelonewolf198927@gmail.com>
2019-02-17 12:04:39 -0500

    freq counter

---
+++ frequencyCounter.js
@@ -0,0 +1,29 @@
+function frequencyCounter(arr1, arr2) {
+    const obj = {};
+    arr1.forEach((ele, index) => {
+        if(arr2.includes(ele**2)) {
+            if (!obj[ele ** 2]) {
+                obj[ele ** 2] = {
+                    count: 1,
+                    element: ele ** 2
+                };
+            } else {
+                obj[ele**2].count += 1
+            }
+        }
+
+    });
+    console.log(obj);
+    for(let i in obj) {
+        if(Object.getOwnPropertyNames(obj).length == arr1.length) {
+            if(obj[i].count > 1) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(frequencyCounter([1, 1, 3], [1, 1, 1]));
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+if (Object.getOwnPropertyNames(obj).length === arr1.length) {
+    //     for (const i in obj) {
+    //         if (ele[i].count > 1) {
+    //             return false;
+    //         }
+    //     }
+    // }
