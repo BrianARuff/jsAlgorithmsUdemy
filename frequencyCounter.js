7a07e0444e39213bec91eaa85ac1a87902365d5b
Brian Ruff <thelonewolf198927@gmail.com>
2019-02-17 12:34:16 -0500

    update with O(n) solution and frequency counter pattern

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -1,3 +1,5 @@
+// o(n^2) solution
+
 function frequencyCounter(arr1, arr2) {
     if(arr1.length !== arr2.length) return false;
     for(let i = 0; i < arr1.length; i++) {
@@ -8,4 +10,28 @@ function frequencyCounter(arr1, arr2) {
     return true;
 }
 
+// o(n) solution
+function frequencyCounter2(arr1, arr2) {
+    if(arr1.length !== arr2.length) return false;
+    const frequencyCounter1 = {};
+    const frequencyCounter2 = {};
+    for(let i of arr1) {
+        frequencyCounter1[i] = (frequencyCounter1[i] || 0) + 1;
+    }
+    for(let i of arr2) {
+        frequencyCounter2[i] = (frequencyCounter2[i] || 0) + 1;
+    }
+    for(let key in frequencyCounter1) {
+        if(!(key ** 2 in frequencyCounter2)) {
+            return false;
+        }
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]) return false;
+    }
+    return true;
+}
+
+
+
 console.log(frequencyCounter([1, 2, 3, 2], [9, 1, 4, 4]));
+console.log(frequencyCounter2([1, 2, 3, 2], [9, 1, 4, 4]));
+
